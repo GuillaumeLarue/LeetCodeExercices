d9388a6ed0456568f454595bbb8585aa46fb1bd4
guillaume.larue <guillaume.larue@epita.fr>
2023-08-09 12:36:31 +0200

    Update longestConsecutive exercice and README.md

--- longestConsecutive/main.py
+++ longestConsecutive/main.py
@@ -9,10 +9,10 @@ class Solution:
             return 0
         res_tmp = 1
         res = 1
-        for i in range(l-1):
-            if (nums[i] - nums[i+1]) == -1 or (nums[i] - nums[i+1]) == 1:
+        for i in range(l - 1):
+            if (nums[i] - nums[i + 1]) == -1 or (nums[i] - nums[i + 1]) == 1:
                 res_tmp += 1
-            elif (nums[i] - nums[i+1]) == 0:
+            elif (nums[i] - nums[i + 1]) == 0:
                 res_tmp += 0
             else:
                 res_tmp = 1
