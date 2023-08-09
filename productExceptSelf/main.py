7f2eafa01744fead272e23c36e870b27c581ad8e
Guillaume <43040692+GuillaumeLarue@users.noreply.github.com>
2023-08-09 18:04:31 +0200

    Update productExceptSelf

--- productExceptSelf/main.py
+++ productExceptSelf/main.py
@@ -4,7 +4,32 @@ from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         l = len(nums)
+        prefix = [0] * l
+        sufix = [0] * l
         res = [0] * l
+        # nums [1, 2, 3, 4]
+        # prefix [1, 2, 6, 24]
+        # sufix [24, 24, 12, 4]
+        # res  [24, 12, 8, 6]
+        for i in range(l):
+            if i == 0:
+                prefix[i] = nums[i]
+            else:
+                prefix[i] = prefix[i-1] * nums[i]
+        for i in range(9, -1, -1):
+        #for i in range(l, 0):
+            if i == l-1:
+                sufix[i] = nums[i]
+            else:
+                sufix[i] = sufix[i+1] * nums[i]
+        for i in range(l):
+            if i <= 0:
+                res[i] = sufix[i + 1]
+            elif i>=l:
+                res[i] = prefix[i - 1]
+            else:
+                res[i] = sufix[i + 1] * prefix[i - 1]
+
         return res
 
     def productExceptSelf2(self, nums: List[int]) -> List[int]:
