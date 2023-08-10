36b00779289f53ac62724177c347476584b93155
guillaume.larue <guillaume.larue@epita.fr>
2023-08-10 22:59:02 +0200

    Update TwoSumII and README.md

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 ## Two pointers
 
 - [x] [Valid Palindrome](https://leetcode.com/problems/valid-palindrome/) (9th August 2023)
-- [ ] [Sum II Input Array Is Sorted](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/) (WIP)
+- [x] [Sum II Input Array Is Sorted](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/) (10th August 2023)
 - [ ] [3Sum](https://leetcode.com/problems/3sum/)
 - [ ] [Container With Most Water](https://leetcode.com/problems/container-with-most-water/)
 - [ ] [Trapping Rain Water](https://leetcode.com/problems/trapping-rain-water/) (WIP)
--- twoSumII/main.py
+++ twoSumII/main.py
@@ -3,15 +3,26 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+        while (numbers[left] + numbers[right]) != target:
+            if (numbers[left] + numbers[right]) > target:
+                right -= 1
+            elif (numbers[left] + numbers[right]) < target:
+                left += 1
+        return [left + 1, right + 1]
+
+    def twoSum2(self, numbers: List[int], target: int) -> List[int]:
+        # Too long
         l = len(numbers)
         mx = l - 1
         mn = 0
         while numbers[mx] > target and mx >= 0 and numbers[mx - 1] >= 0:
             mx -= 1
-            #print(mx)
+            # print(mx)
         while numbers[mn] < -target and mn < l - 1 and numbers[mn + 1] >= 0:
             mn += 1
-            #print(mn)
+            # print(mn)
         for i in range(mx + 1):
             for j in range(mn, mx + 1):
                 # mn-1 ?
