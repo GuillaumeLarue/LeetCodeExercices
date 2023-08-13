52b97770c48ebb6afc6c54bbb234143501ed8123
guillaume.larue <guillaume.larue@epita.fr>
2023-08-13 20:24:28 +0200

    Update containerWithMostWater

--- containerWithMostWater/main.py
+++ containerWithMostWater/main.py
@@ -3,7 +3,19 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        return [1]
+        le = len(height)
+        res = 0
+        l = 0
+        r = le - 1
+        while l < r:
+            res_tmp = min(height[l], height[r]) * (r - l)
+            res = max(res, res_tmp)
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+        return res
+
 
     def maxArea2(self, height: List[int]) -> int:
         # Too long
