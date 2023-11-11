a7f809fc16d939b8861290a6a821c4a9ab396821
guillaume.larue <guillaume.larue@epita.fr>
2023-11-11 17:17:40 +0100

    Add 2 method for mergeTwoLinkedLists

--- mergeTwoLists/main.py
+++ mergeTwoLists/main.py
@@ -5,7 +5,23 @@ from reverseList.main import ListNode
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        return None
+        # In place method
+        head = ListNode()
+        curr = head
+
+        while list1 is not None and list2 is not None:
+            if list1.value > list2.value:
+                curr.next = list2
+                list2 = list2.next
+            else:
+                curr.next = list1
+                list1 = list1.next
+            curr = curr.next
+        if list1 and not list2:
+            curr.next = list1
+        else:
+            curr.next = list2
+        return head.next
 
     def mergeTwoLists_1(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         # To complex, not in place
